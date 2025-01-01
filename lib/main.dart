9f0e4e0ec2703367343c711a5e64853a3d72d22d
hendroprwk08 <40080703+hendroprwk08@users.noreply.github.com>
2025-01-01 07:05:19 +0700

    ok

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,7 @@ class MyApp extends StatelessWidget {
             color: Colors.white,
           ),
         ),
+        scaffoldBackgroundColor: Colors.white,
         textTheme: const TextTheme(
           titleLarge: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
           titleMedium: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
@@ -39,6 +40,12 @@ class MyApp extends StatelessWidget {
             fontSize: 14,
           ),
         ),
+        floatingActionButtonTheme: const FloatingActionButtonThemeData(
+          shape: CircleBorder(),
+          backgroundColor: Colors.orange,
+        ),
+        textButtonTheme: TextButtonThemeData(
+            style: TextButton.styleFrom(foregroundColor: Colors.blue)),
         outlinedButtonTheme: OutlinedButtonThemeData(
           style: OutlinedButton.styleFrom(
               side: const BorderSide(color: Colors.blue),
@@ -83,13 +90,13 @@ class MyApp extends StatelessWidget {
 
     ThemeData myDarkTheme = ThemeData(
         useMaterial3: true,
-        primaryColor: const Color(0x0009192a),
-        hintColor: Colors.grey,
+        primaryColor: Color(0xff283449),
+        hintColor: Color(0xffffc054),
         fontFamily: 'Roboto',
         colorScheme: ColorScheme.fromSeed(
-            seedColor: Colors.blue, brightness: Brightness.light),
+            seedColor: Colors.indigo, brightness: Brightness.dark),
         appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.blue,
+          backgroundColor: Color(0xff283449), // diawali 0xff
           titleTextStyle: TextStyle(
             color: Colors.white,
             fontSize: 20.0,
@@ -99,63 +106,60 @@ class MyApp extends StatelessWidget {
             color: Colors.white,
           ),
         ),
+        scaffoldBackgroundColor: Color(0xff3d485b),
         textTheme: const TextTheme(
-          titleLarge: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
-          titleMedium: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-          titleSmall: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-          bodyLarge: TextStyle(
-            fontSize: 20,
-          ),
-          bodyMedium: TextStyle(
-            fontSize: 14,
-          ),
-          bodySmall: TextStyle(
-            fontSize: 14,
-          ),
+          titleLarge: TextStyle(
+              fontSize: 28, fontWeight: FontWeight.bold, color: Colors.white70),
+          titleMedium: TextStyle(
+              fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white70),
+          titleSmall: TextStyle(
+              fontSize: 16, fontWeight: FontWeight.bold, color: Colors.white70),
+          bodyLarge: TextStyle(fontSize: 20, color: Colors.white70),
+          bodyMedium: TextStyle(fontSize: 14, color: Colors.white70),
+          bodySmall: TextStyle(fontSize: 14, color: Colors.white70),
         ),
         floatingActionButtonTheme: const FloatingActionButtonThemeData(
           shape: CircleBorder(),
-          backgroundColor: Colors.orange,
+          backgroundColor: Color(0xff633e73),
+        ),
+        textButtonTheme: TextButtonThemeData(
+            style: TextButton.styleFrom(foregroundColor: Colors.white70)),
+        outlinedButtonTheme: OutlinedButtonThemeData(
+          style: OutlinedButton.styleFrom(
+              side: const BorderSide(color: Colors.white70),
+              foregroundColor: Colors.white70),
         ),
         elevatedButtonTheme: ElevatedButtonThemeData(
             style: ButtonStyle(
-          backgroundColor: WidgetStateProperty.all<Color>(Colors.blue),
-          foregroundColor: WidgetStateProperty.all<Color>(Colors.white),
+          backgroundColor: WidgetStateProperty.all<Color>(Color(0xff283449)),
+          foregroundColor: WidgetStateProperty.all<Color>(Colors.white70),
         )),
-        outlinedButtonTheme: OutlinedButtonThemeData(
-          style: OutlinedButton.styleFrom(
-              side: const BorderSide(color: Colors.blue),
-              foregroundColor: Colors.blue),
-        ),
         inputDecorationTheme: InputDecorationTheme(
           filled: true,
-          fillColor: Colors.white,
+          fillColor: Color(0xff283449),
           contentPadding: const EdgeInsets.symmetric(horizontal: 16),
-          labelStyle: const TextStyle(
-            fontSize: 15,
-          ),
-          hintStyle: const TextStyle(
-            fontSize: 10,
-          ),
+          labelStyle: const TextStyle(fontSize: 15, color: Colors.white54),
+          hintStyle: const TextStyle(fontSize: 15, color: Color(0xff633e73)),
+          floatingLabelStyle: TextStyle(fontSize: 20, color: Color(0xff633e73)),
           border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(8),
             borderSide: BorderSide.none,
           ),
           enabledBorder: OutlineInputBorder(
             borderRadius: BorderRadius.circular(8),
-            borderSide: const BorderSide(color: Colors.grey),
+            borderSide: const BorderSide(color: Color(0xff633e73)),
           ),
           focusedBorder: OutlineInputBorder(
             borderRadius: BorderRadius.circular(8),
-            borderSide: const BorderSide(color: Colors.blue),
+            borderSide: const BorderSide(color: Color(0xff633e73)),
           ),
           errorBorder: OutlineInputBorder(
             borderRadius: BorderRadius.circular(8),
-            borderSide: const BorderSide(color: Colors.red),
+            borderSide: const BorderSide(color: Colors.orangeAccent),
           ),
           focusedErrorBorder: OutlineInputBorder(
             borderRadius: BorderRadius.circular(8),
-            borderSide: const BorderSide(color: Colors.red),
+            borderSide: const BorderSide(color: Colors.orangeAccent),
           ),
         ));
 
@@ -163,7 +167,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: myLightTheme,
-      // darkTheme: myDarkTheme,
+      darkTheme: myDarkTheme,
       home: const MyHomePage(title: 'Flutter: Theme'),
     );
   }
